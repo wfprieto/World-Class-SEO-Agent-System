c0be96289e8169d6158ff7ba5f7fe9c6d479227e
wfprieto <wfprieto@gmail.com>
2026-07-11 21:18:42 -0400

    feat(cli): add seoctl package

---
+++ seoctl/__init__.py
@@ -0,0 +1,11 @@
+"""Unified operator command surface for the World-Class SEO Agent System."""
+
+from .cli import EXIT_BLOCKED, EXIT_FAILED, EXIT_INPUT, EXIT_OK, EXIT_UNAVAILABLE
+
+__all__ = [
+    "EXIT_OK",
+    "EXIT_INPUT",
+    "EXIT_UNAVAILABLE",
+    "EXIT_BLOCKED",
+    "EXIT_FAILED",
+]
