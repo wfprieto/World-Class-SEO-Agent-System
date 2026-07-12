cb67d9ebe52fba60e45143a367572f55e9fefc93
wfprieto <wfprieto@gmail.com>
2026-07-11 22:14:19 -0400

    chore(google): register current CrUX and sitemap adapters

--- adapters/registry.py
+++ adapters/registry.py
@@ -10,6 +10,7 @@ from adapters.crawler_csv import CrawlerCSVAdapter
 from adapters.ga4 import GA4ExportAdapter
 from adapters.gsc import GSCExportAdapter
 from adapters.gbp_local import GBPLocalAdapter
+from adapters.google_pagespeed_live import GooglePageSpeedLiveAdapter
 from adapters.hreflang_validator import HreflangValidatorAdapter
 from adapters.knowledge_graph import KnowledgeGraphAdapter
 from adapters.log_parser import LogParserAdapter
@@ -18,12 +19,12 @@ from adapters.rank_tracking import RankTrackingCSVAdapter
 from adapters.redirect_chain import RedirectChainAdapter
 from adapters.rendered_page import RenderedPageAdapter
 from adapters.robots_txt import RobotsTxtAdapter
-from adapters.google_pagespeed_live import GooglePageSpeedLiveAdapter
 from adapters.schema_validation import SchemaValidationAdapter
 from adapters.sitemap_validator import SitemapValidatorAdapter
-from integrations.google.crux import CrUXHistoryAdapter
+from integrations.google.crux import CrUXCurrentAdapter, CrUXHistoryAdapter
 from integrations.google.ga4 import GoogleAnalyticsDataAdapter
 from integrations.google.gsc import GoogleSearchConsoleAdapter
+from integrations.google.sitemaps import GoogleSitemapsAdapter
 
 
 def default_adapters() -> dict[str, object]:
@@ -44,6 +45,8 @@ def default_adapters() -> dict[str, object]:
         "gsc_export": GSCExportAdapter(),
         "gsc_live": GoogleSearchConsoleAdapter(),
         "google_search_console": GoogleSearchConsoleAdapter(),
+        "google_search_console_sitemaps": GoogleSitemapsAdapter(),
+        "gsc_sitemaps": GoogleSitemapsAdapter(),
         "hreflang_validator": HreflangValidatorAdapter(),
         "knowledge_graph": KnowledgeGraphAdapter(),
         "log_parser": LogParserAdapter(),
@@ -58,6 +61,8 @@ def default_adapters() -> dict[str, object]:
         "sitemap_validator": SitemapValidatorAdapter(),
         "google_pagespeed_live": GooglePageSpeedLiveAdapter(),
         "pagespeed_live": GooglePageSpeedLiveAdapter(),
+        "google_crux_current": CrUXCurrentAdapter(),
+        "crux_current": CrUXCurrentAdapter(),
         "google_crux_history": CrUXHistoryAdapter(),
         "crux_history": CrUXHistoryAdapter(),
     }
